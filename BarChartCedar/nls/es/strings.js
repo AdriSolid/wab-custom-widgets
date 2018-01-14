16469ad5885b8525b986b6a1774c41b0cdf5d4f1
AdriSolid <biotekgis@gmail.com>
2018-01-14 20:03:30 +0100

    File deleted

--- BarChartCedar/nls/es/strings.js
+++
@@ -1,8 +0,0 @@
-define({
-  selectLayer: "Selecciona una capa",
-  selectXField: "Selecciona el campo X",
-  selectYField: "Selecciona el campo Y",
-  selectChart: "Selecciona un tipo de gráfico",
-  execute: "Ejecutar",
-  extent: "Filtrar por extensión"
-});
