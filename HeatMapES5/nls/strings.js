ecd37d4f63555bb27b607e8a98411ac15d836a96
AdriSolid <biotekgis@gmail.com>
2018-04-26 10:11:06 +0200

    Setting nls strings

--- HeatMapES5/nls/strings.js
+++ HeatMapES5/nls/strings.js
@@ -2,7 +2,17 @@ define({
   root: ({
     _widgetLabel: "HeatMap",
     selectLayer: "Select a layer",
-    selectField: "Select a numeric field"
+    selectField: "Select a numeric field",
+    blurRadius: "Blur Radius",
+    maxValue: "Max Value",
+    minValue: "Min Value",
+    heatMapSlider: "Heat Map Slider",
+    execute: "Execute",
+    removeButton: "Remove all HeatMap layers",
+    alertTitle: "Warning!",
+    alertContent: "Do you want to remove all the heatMap layers?",
+    alertButtonOk: "Yes",
+    alertButtonCancel: "No"
   }),
   "es": 1
 });
