eb35a699447629096efa974f283f42e3d5f42629
AdriSolid <biotekgis@gmail.com>
2018-04-26 10:13:01 +0200

    Setting nls strings

--- HeatMapES5/nls/es/strings.js
+++ HeatMapES5/nls/es/strings.js
@@ -1,5 +1,15 @@
 ﻿define({
     _widgetLabel: "HeatMap",
     selectLayer: "Selecciona una capa",
-    selectField: "Selecciona un campo numérico"
+    selectField: "Selecciona un campo numérico",
+    blurRadius: "Blur Radius",
+    maxValue: "Max Value",
+    minValue: "Min Value",
+    heatMapSlider: "Heat Map Slider",
+    execute: "Ejecutar",
+    removeButton: "Remover todas las capas de calor",
+    alertTitle: "Peligro!",
+    alertContent: "¿Quieres remover todas las capas de valor?",
+    alertButtonOk: "Si",
+    alertButtonCancel: "No"
 });
