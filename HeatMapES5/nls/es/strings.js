c7b4bc432e94535d284cb1a4d7add55d8d4e26e3
AdriSolid <biotekgis@gmail.com>
2018-05-10 19:10:11 +0200

    Updated

--- HeatMapES5/nls/es/strings.js
+++ HeatMapES5/nls/es/strings.js
@@ -1,11 +1,15 @@
-﻿define({
+define({
     _widgetLabel: "HeatMap",
     selectLayer: "Selecciona una capa",
     selectField: "Selecciona un campo numérico",
     blurRadius: "Blur Radius",
     maxValue: "Max Value",
     minValue: "Min Value",
+    drawingTools: "Herramientas de dibujo",
+    clearGraphics: "Limpiar",
     heatMapSlider: "Heat Map Slider",
+    clearGraphicsButton: "Limpiar",
+    drawingTools: "Herramientas de dibujo",
     execute: "Ejecutar",
     removeButton: "Remover todas las capas de calor",
     alertTitle: "Peligro!",
