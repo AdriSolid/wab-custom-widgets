c0aa2de782d077a6a9219223305879d7815c3690
AdriSolid <biotekgis@gmail.com>
2018-06-18 19:59:09 +0200

    :bar_chart:

--- chartJS/Widget.js
+++ chartJS/Widget.js
@@ -2,188 +2,186 @@ define(['dojo/_base/declare',
         'jimu/BaseWidget',
         'dojo/on',
         'dojo/_base/lang',
+        'dojo/Deferred',
+        'dojo/dom',
         'dijit/form/Select',
         'dijit/form/Button',
-        'dijit/form/CheckBox',
+        'dijit/layout/TabContainer',
+        'dijit/layout/ContentPane',
+        './chartJS',
         './webMapLayersIds',
         'esri/tasks/query', 
         'esri/tasks/QueryTask',
-        'dojo/dom',
+        'jimu/dijit/TabContainer3',
         'dojo/domReady!'],
-function(declare, BaseWidget, on, lang, Select, Button, CheckBox, webMapLayersIds, Query, QueryTask, dom) {
+function(declare, BaseWidget, on, lang, Deferred, dom, 
+         Select, Button, TabContainer, ContentPane,
+         chartJS, webMapLayersIds, 
+         Query, QueryTask,
+         TabContainer3) {
   
   return declare([BaseWidget], {
 
+    layer: null,
     fieldX: null,
     fieldY: null,
     url: null,
     type: null,
-    chart: null,
+    lineChart: null,
+    barChart: null,
+    extensionFilter: null,
+    extensionEvent: null,
 
     startup: function() {
         this.inherited(arguments)
+        this.initTabs()
         this.initLayerChooser()
-        this.initButton()
+        this.initCharts()
+    },
+
+    initCharts: function(){
+      this.lineChart = new chartJS({
+          node: this.canvas,
+          type: 'line',
+          labels: null,
+          label: this.fieldY,
+          data: null
+        })
+
+      this.barChart = new chartJS({
+        node: this.barCanvas,
+        type: 'bar',
+        labels: null,
+        label: this.fieldY,
+        data: null
+      })
+    },
+
+    initTabs: function(){
+      var lineTab = {
+        title: this.nls.lineTitleTab,
+        content: this.lineTabNode
+      };
+
+      var barTab = {
+        title: this.nls.barTitleTab,
+        content: this.barTabNode
+      };
+
+      var tab = new TabContainer3({
+        tabs: [lineTab, barTab]
+      }, this.tabNode);
+    },
+
+    onOpen: function(){
+      this.extensionEvent = this.map.on('extent-change', lang.hitch(this, function(evt){
+        this.extensionFilter = evt.extent
+        this.gettingLayer()
+      }));
     },
 
     initLayerChooser: function(){
-        var idForChangeEvent = "layerChooserNodeEvent";
+      var idForChangeEvent = "layerChooserNodeEvent";
 
-        new webMapLayersIds({
-          idForChangeEvent: idForChangeEvent,
-          layerNode: "layerChooserNode",
-          map: this.map,
-          geometry: "*"
-        });
+      new webMapLayersIds({
+        idForChangeEvent: idForChangeEvent,
+        layerNode: "layerChooserNode",
+        map: this.map,
+        geometry: "*"
+      });
 
-        this.initSelects(dijit.byId(idForChangeEvent).value);
+      this.initSelects(dijit.byId(idForChangeEvent).value);
 
-        dijit.byId(idForChangeEvent).on("change", lang.hitch(this, function(evt){
-          this.options(evt)
-        }))
-      },
+      dijit.byId(idForChangeEvent).on("change", lang.hitch(this, function(evt){
+        this.options(evt)
+      }))
+    },
 
-      initSelects: function(layerId){
-        new Select({
-          name: "selectFieldX",
-          id: "selectFieldX"
-        }).placeAt('selectXContainer').startup()
+    initSelects: function(layerId){
+      new Select({
+        name: "selectFieldX",
+        id: "selectFieldX"
+      }, this.selectXContainer).startup()
 
-        new Select({
-            name: "selectFieldY",
-            id: "selectFieldY"
-        }).placeAt('selectYContainer').startup()
+      new Select({
+          name: "selectFieldY",
+          id: "selectFieldY"
+      }, this.selectYContainer).startup()
 
-        new Select({
-            name: "selectTypeChart",
-            id: "_typeChart",
-            options: [{label: "Line", value: "line"}, 
-                      {label: "Bar", value: "bar"}]
-          }).placeAt('typeChartContainer').startup()
+      var fieldXid = dijit.byId('selectFieldX')
+      this.fieldX = fieldXid.value;
+      this.own(on(fieldXid, 'change', lang.hitch(this, function(evt){
+        this.fieldX = evt
+      })));
 
-        var fieldXid = dijit.byId('selectFieldX')
-        this.fieldX = fieldXid.value;
-        fieldXid.on("change", lang.hitch(this, function(evt){
-            this.fieldX = evt
-        }))
+      var fieldYid = dijit.byId('selectFieldY')
+      this.fieldY = fieldYid.value;
+      this.own(on(fieldYid, 'change', lang.hitch(this, function(evt){
+        this.fieldY = evt
+      })));
 
-        var fieldYid = dijit.byId('selectFieldY')
-        this.fieldY = fieldYid.value;
-        fieldYid.on("change", lang.hitch(this, function(evt){
-            this.fieldY = evt
-        }))
+      this.options(layerId)
+    },
 
-        var typeChartId = dijit.byId('_typeChart')
-        this.type = typeChartId.value
-        typeChartId.on("change", lang.hitch(this, function(evt){
-            this.type = evt
-        }))
+    options: function(layerId){
+      this.layer = this.map.getLayer(layerId)
+      this.url = this.layer.url
+      var fields = this.layer.fields
 
-        this.options(layerId)
-      },
-
-      options: function(layerId){
-        var layer = this.map.getLayer(layerId)
-        this.url = layer.url
-        var fields = layer.fields
-
-        var map = fields.map(function(record){
-          return dojo.create("option", {
-            label: record.name,
-            value: record.name
-          })
+      var map = fields.map(function(record){
+        return dojo.create("option", {
+          label: record.name,
+          value: record.name
         })
+      })
 
-        var selectX = dijit.byId('selectFieldX')
-        var selectY = dijit.byId('selectFieldY')
+      var selectX = dijit.byId('selectFieldX')
+      var selectY = dijit.byId('selectFieldY')
 
-        if(selectX.getOptions()){
-          selectX.removeOption(selectX.getOptions())
-          selectY.removeOption(selectY.getOptions())
-          selectX.addOption(map)
-          selectY.addOption(map)
-        }
-      },
-
-      initButton: function(){
-        new Button({
-          label: this.nls.execute,
-          onClick: lang.hitch(this, function(){
-              this.gettingLayer()
-              this.updateChart(this.type)
-          })
-        }, "executeChart").startup()
-      },
-
-      gettingLayer: function(){
-        var query = new Query()
-            query.where = "1=1"
-            query.returnGeometry = false
-            query.outFields = [this.fieldX, this.fieldY]
-            new QueryTask(this.url).execute(query, lang.hitch(this, function(results){
-              this.render(results)
-            }))
-      },
-
-      render: function(results){
-        var map = results.features.map(lang.hitch(this, function(record){
-          return {
-            x: record.attributes[this.fieldX],
-            y: record.attributes[this.fieldY]
-          }
-        }))
-      
-        var labels = map.map(lang.hitch(this, function(record){
-          return record.x
-        }))
-      
-        this.chart = new Chart(this.canvas.getContext('2d'), { 
-          type: this.type, 
-          data: { 
-              labels: labels, 
-              datasets: [{  
-                  label: this.fieldY, 
-                  fill: false,
-                  backgroundColor: 'rgb(51, 173, 255)', 
-                  borderColor: 'rgb(0, 138, 230)', 
-                  borderWidth: 1,
-                  pointRadius: 2,
-                  data: map, 
-              }] 
-          }, 
-          options: {}
-         })
-      },
-
-      updateChart: function(){ 
-        if(this.chart){
-          this.chart.destroy()
-        }
+      if(selectX.getOptions()){
+        selectX.options.length = 0
+        selectY.options.length = 0
+        selectX.addOption(map)
+        selectY.addOption(map)
       }
+    },
 
-    /*onOpen: function(){
-      console.log('onOpen');
+    gettingLayer: function(){
+      var query = new Query()
+          query.where = "1=1"
+          query.geometry = this.extensionFilter
+          query.returnGeometry = false
+          query.outFields = [this.fieldX, this.fieldY]
+          new QueryTask(this.url).execute(query, lang.hitch(this, function(results){
+            this.render(results)
+          }))
+    },
+
+    render: function(results){
+      var map = []
+      var labels = []
+
+      var def = new Deferred()
+          def.resolve(':)');
+          def.then(lang.hitch(this, function(){
+            for(i in results.features){
+              map.push({
+                x: results.features[i].attributes[this.fieldX],
+                y: results.features[i].attributes[this.fieldY]
+              })
+            }
+          })).then(lang.hitch(this, function(){
+            for(i in map){
+              labels.push(map[i].x)
+            }
+          })).then(lang.hitch(this, function(){
+            this.lineChart.updateChart(labels, map)
+            this.barChart.updateChart(labels, map)
+          }))
     },
 
     onClose: function(){
-      console.log('onClose');
-    },
-
-    onMinimize: function(){
-      console.log('onMinimize');
-    },
-
-    onMaximize: function(){
-      console.log('onMaximize');
-    },
-
-    onSignIn: function(credential){
-         //jshint unused:false
-      console.log('onSignIn');
-    },
-
-    onSignOut: function(){
-      console.log('onSignOut');
-    }*/
+      this.extensionEvent.remove()
+    }
   });
 });
