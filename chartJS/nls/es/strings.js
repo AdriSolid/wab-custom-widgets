8170ca5f54fc87161102b8ddfd328f04512329e4
AdriSolid <biotekgis@gmail.com>
2018-06-18 19:56:50 +0200

    :bar_chart:

--- chartJS/nls/es/strings.js
+++ chartJS/nls/es/strings.js
@@ -1,7 +1,9 @@
-﻿define({
+define({
   selectLayer: "Selecciona una capa",
   selectXField: "Selecciona el campo X",
   selectYField: "Selecciona el campo Y",
   selectChart: "Tipo de gráfico",
-  execute: "Ejecutar"
+  execute: "Ejecutar",
+  lineTitleTab: "Líneas",
+  barTitleTab: "Barras"
 });
